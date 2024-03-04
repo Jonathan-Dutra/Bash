8309d388103202727bd3b8092d185bd6b2efa7fa
Jonathan N. Dutra <dutra_jonathan@hotmail.com>
2024-03-04 15:01:56 -0300

    Atualizado aquivo novo_script

--- teste.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# Nome: teste
-# Descricao: 
-# Autor:  Jonathan Nasc. Dutra < jonatthan.dutra@gmail.com >
-# OBS: Arquivo criado com base nos videos do Blau : https://www.youtube.com/@debxp
-# Versao: 0001
